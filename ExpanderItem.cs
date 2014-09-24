6c8a2ac3d3f3101f9714d4951066f99b5a43dabc
razelda <Razelda@gmail.com>
2014-09-24 01:20:25 -0500

    Create ExpanderItem.cs

---
+++ ExpanderItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Copy_IT
+{
+    class ExpanderItem
+    {
+        public string Header { get; set; }
+        public string ItemId { get; set; }
+        public FrameworkElement Content { get; set; }
+    }
+}
